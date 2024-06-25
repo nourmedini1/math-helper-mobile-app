d91f27089052b5bd6b39b8bd5281d61eb1527a61
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-25 16:58:23 +0100

    added api failure classes

--- math_helper/lib/core/errors/failure.dart
+++ math_helper/lib/core/errors/failure.dart
@@ -8,3 +8,40 @@ class Failure extends Equatable {
   @override
   List<Object> get props => [message];
 }
+
+class BadRequestFailure extends Failure {
+  final String failureMessage;
+
+  const BadRequestFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
+
+class TimeoutFailure extends Failure {
+  final String failureMessage;
+  const TimeoutFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
+
+class ConstraintViolationFailure extends Failure {
+  final String failureMessage;
+  const ConstraintViolationFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
+
+class ServerFailure extends Failure {
+  final String failureMessage;
+  const ServerFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
+
+class ConnectionUnavailableFailure extends Failure {
+  final String failureMessage;
+  const ConnectionUnavailableFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
+
+class UnexpectedFailure extends Failure {
+  final String failureMessage;
+  const UnexpectedFailure({required this.failureMessage})
+      : super(message: failureMessage);
+}
