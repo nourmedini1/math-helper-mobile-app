b362602cdd07c3fa7a1db9ea77a3262245c074cf
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-25 16:58:10 +0100

    added exception messages

--- math_helper/lib/core/messages.dart
+++ math_helper/lib/core/messages.dart
@@ -1,2 +1,4 @@
 const timeoutMessage = "Time limit for a response was exceeded";
 const unexpectedErrorMessage = "An unexpected error occured";
+const connectionUnavailableMessage = "Please check your internet connection";
+const serverFailureMessage = "Our services are temporarly unavailable";
