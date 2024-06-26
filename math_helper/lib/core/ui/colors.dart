eea03a985ff3e45181145b9a12543fcd870d5878
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-26 15:55:05 +0100

    added the colors needed for the app

---
+++ math_helper/lib/core/ui/colors.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/animation.dart';
+
+class Colors {
+  static const Color primaryColor = Color(0xff788a65);
+  static const Color primaryColorTint10 = Color(0xff869674);
+  static const Color primaryColorTint20 = Color(0xff93a184);
+  static const Color primaryColorTint30 = Color(0xffa1ad93);
+  static const Color primaryColorTint40 = Color(0xffaeb9a3);
+  static const Color primaryColorTint50 = Color(0xffbcc5b2);
+  static const Color primaryColorShade10 = Color(0xff6c7c5b);
+  static const Color primaryColorShade20 = Color(0xff606e51);
+  static const Color primaryColorShade30 = Color(0xff546147);
+  static const Color primaryColorShade40 = Color(0xff48533d);
+  static const Color primaryColorShade50 = Color(0xff3c4533);
+
+  static const Color secondaryColor = Color(0xffd4cbbd);
+  static const Color secondaryColorTint10 = Color(0xffd8d0c4);
+  static const Color secondaryColorTint20 = Color(0xffddd5ca);
+  static const Color secondaryColorTint30 = Color(0xffe1dbd1);
+  static const Color secondaryColorTint40 = Color(0xffe5e0d7);
+  static const Color secondaryColorTint50 = Color(0xffeae5de);
+  static const Color secondaryColorShade10 = Color(0xffbfb7aa);
+  static const Color secondaryColorShade20 = Color(0xffaaa297);
+  static const Color secondaryColorShade30 = Color(0xff948e84);
+  static const Color secondaryColorShade40 = Color(0xff7f7a71);
+  static const Color secondaryColorShade50 = Color(0xff6a665f);
+
+  static const Color customBlack = Color(0xff000000);
+  static const Color customBlackTint20 = Color(0xff333333);
+  static const Color customBlackTint40 = Color(0xff666666);
+  static const Color customBlackTint60 = Color(0xff999999);
+  static const Color customBlackTint80 = Color(0xffcccccc);
+  static const Color customWhite = Color(0xffffffff);
+
+  static const Color customPampas = Color(0xffefebe7);
+  static const Color customDarkGrey = Color(0xff2c2c2c);
+
+  static const Color customRed = Color(0xffb00020);
+  static const Color customGreen = Color(0xff00c853);
+  static const Color customYellow = Color(0xffffd600);
+}
