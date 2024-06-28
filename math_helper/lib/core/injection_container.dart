df6fa6d6ad85d35a969ad34839d11aab5e1db727
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-28 10:09:59 +0100

    added the dependency injections for the derivatives feature

--- math_helper/lib/core/injection_container.dart
+++ math_helper/lib/core/injection_container.dart
@@ -12,6 +12,13 @@ import 'package:math_helper/features/complex/presentation/bloc/complex_addition/
 import 'package:math_helper/features/complex/presentation/bloc/complex_multiplication/complex_multiplication_bloc.dart';
 import 'package:math_helper/features/complex/presentation/bloc/complex_substraction/complex_substraction_bloc.dart';
 import 'package:math_helper/features/complex/presentation/bloc/polar_form/polar_form_bloc.dart';
+import 'package:math_helper/features/derivatives/data/api/derivatives_api.dart';
+import 'package:math_helper/features/derivatives/data/repository/derivatives_repository_impl.dart';
+import 'package:math_helper/features/derivatives/domain/repository/derivatives_repository.dart';
+import 'package:math_helper/features/derivatives/domain/usecases/numeric_derivative_usecase.dart';
+import 'package:math_helper/features/derivatives/domain/usecases/symbolic_derivative_usecase.dart';
+import 'package:math_helper/features/derivatives/presentation/bloc/numeric_derivative/numeric_derivative_bloc.dart';
+import 'package:math_helper/features/derivatives/presentation/bloc/symbolic_derivative/symbolic_derivative_bloc.dart';
 
 final ic = GetIt.instance;
 
@@ -41,4 +48,19 @@ Future<void> init() async {
   ic.registerFactory(
       () => ComplexMultiplicationBloc(complexMultiplicationUsecase: ic()));
   ic.registerFactory(() => PolarFormBloc(polarFormUsecase: ic()));
+
+  // derivatives
+  ic.registerLazySingleton(() => DerivativesApi(client: ic()));
+  ic.registerLazySingleton<DerivativesRepository>(() =>
+      DerivativesRepositoryImpl(connectivity: ic(), derivativesApi: ic()));
+
+  ic.registerLazySingleton(
+      () => SymbolicDerivativeUsecase(derivativesRepository: ic()));
+  ic.registerLazySingleton(
+      () => NumericDerivativeUsecase(derivativesRepository: ic()));
+
+  ic.registerFactory(
+      () => SymbolicDerivativeBloc(symbolicDerivativeUsecase: ic()));
+  ic.registerFactory(
+      () => NumericDerivativeBloc(numericDerivativeUsecase: ic()));
 }
