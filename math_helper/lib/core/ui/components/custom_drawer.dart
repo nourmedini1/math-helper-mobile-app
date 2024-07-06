fa102733456e3eb771f59804af5cca31cad3874e
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-07-06 09:04:26 +0100

    created a custom drawer

---
+++ math_helper/lib/core/ui/components/custom_drawer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:math_helper/core/strings.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).drawerTheme.backgroundColor,
+      width: MediaQuery.of(context).size.width * (2 / 3),
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+              child: Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Text(
+              Strings.appName,
+              style: Theme.of(context).appBarTheme.titleTextStyle,
+            ),
+          )),
+          ListTile(
+            title: Text(
+              Strings.drawerItemTitleRecentOperations,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
