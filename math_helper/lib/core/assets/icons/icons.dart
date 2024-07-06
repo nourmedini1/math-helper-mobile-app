bf64c3836f7f7a51cfb4c6cd75c632d21eea67af
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-07-06 09:00:32 +0100

    added icons for the integral math topic

---
+++ math_helper/lib/core/assets/icons/icons.dart
@@ -0,0 +1,7 @@
+class CustomIcons {
+  static const String singlePrimitive =
+      "lib/core/assets/icons/single-primitive.svg";
+  static const String definiteIntegral =
+      "lib/core/assets/icons/definite_integral.svg";
+  static const String integral = "lib/core/assets/icons/integral.png";
+}
