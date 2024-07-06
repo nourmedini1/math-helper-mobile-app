a57309a17fffde3832696c741ba90651bc4c25c6
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-07-06 08:59:58 +0100

    added ubuntu bold font

---
+++ math_helper/lib/core/assets/fonts/fonts.dart
@@ -0,0 +1,7 @@
+class CustomFonts {
+  static const String ubuntuBold = "ubuntuBold";
+  static const String robotoBold = "robotoBold";
+  static const String robotoLight = "robotoLight";
+  static const String robotoMedium = "robotoMedium";
+  static const String robotoRegular = "robotoRegular";
+}
