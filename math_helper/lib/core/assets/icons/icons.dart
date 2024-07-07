24b23071cba582e27d6070537b4daf37332e7892
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-07-07 18:24:38 +0100

    added the icons to be used in the home page

--- math_helper/lib/core/assets/icons/icons.dart
+++ math_helper/lib/core/assets/icons/icons.dart
@@ -4,4 +4,27 @@ class CustomIcons {
   static const String definiteIntegral =
       "lib/core/assets/icons/definite_integral.svg";
   static const String integral = "lib/core/assets/icons/integral.png";
+  static const String differentialCalculus =
+      "lib/core/assets/icons/differential-calculus.png";
+  static const String derivative = "lib/core/assets/icons/derivative.png";
+  static const String partialDerivative =
+      "lib/core/assets/icons/partial-derivative.png";
+  static const String differentialEquation =
+      "lib/core/assets/icons/differential-equation.png";
+  static const String complexAnalysis =
+      "lib/core/assets/icons/complex-analysis.png";
+  static const String polarForm = "lib/core/assets/icons/polar-form.png";
+  static const String operations = "lib/core/assets/icons/operations.png";
+  static const String binomial = "lib/core/assets/icons/binomial.png";
+  static const String summation = "lib/core/assets/icons/summation.webp";
+  static const String product = "lib/core/assets/icons/product.png";
+  static const String taylorSeries = "lib/core/assets/icons/taylor-series.png";
+  static const String limit = "lib/core/assets/icons/limit.webp";
+  static const String limits = "lib/core/assets/icons/limits.png";
+  static const String algebra = "lib/core/assets/icons/algebra.png";
+  static const String matrix = "lib/core/assets/icons/matrix.png";
+  static const String rank = "lib/core/assets/icons/rank.png";
+  static const String linearEquation =
+      "lib/core/assets/icons/linear-equation.png";
+  static const String determinant = "lib/core/assets/icons/determinant.png";
 }
