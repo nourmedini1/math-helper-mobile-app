df620efcd59c75de06a024d8381cb96130b7669d
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-07-24 08:10:33 +0100

    created the sum screen

--- math_helper/lib/features/sum/data/models/sum_response.dart
+++ math_helper/lib/features/sum/data/models/sum_response.dart
@@ -1,24 +1,24 @@
 import 'package:equatable/equatable.dart';
 
 class SumResponse extends Equatable {
-  final String product;
+  final String summation;
   final String result;
   final bool convergent;
 
   const SumResponse({
-    required this.product,
+    required this.summation,
     required this.result,
     required this.convergent,
   });
 
   factory SumResponse.fromJson(Map<String, dynamic> json) {
     return SumResponse(
-      product: json['product'],
+      summation: json['summation'],
       result: json['result'],
       convergent: json['convergent'],
     );
   }
 
   @override
-  List<Object?> get props => [product, result, convergent];
+  List<Object?> get props => [summation, result, convergent];
 }
